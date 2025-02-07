50254da764544f19aa17d919e443e1dd4c4861d2
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-02-07 00:54:18 -0600

    Relacion entre componentes e inyeccion de valores con @Value desde archivo properties

--- src/main/java/com/eduardo/springboot/di/factura/springboot_difactura/models/Item.java
+++ src/main/java/com/eduardo/springboot/di/factura/springboot_difactura/models/Item.java
@@ -1,5 +1,20 @@
 package com.eduardo.springboot.di.factura.springboot_difactura.models;
 
 public class Item {
-    
+    private Product product;
+    private Integer quantity;
+
+    public Product getproduct(){
+        return product;
+    }
+    public void setProduct(Product product){
+        this.product = product;
+    }
+
+    public Integer getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(Integer quantity){
+        this.quantity = quantity;
+    }
 }
